5d1e6f2f7d81919065b5baa704333caa192b1918
Robinson <noelrobinsonndecky@gmail.com>
2025-01-29 18:13:49 +0000

    update master 29/01/25

--- src/app/shared/footer-page/footer-page.component.ts
+++ src/app/shared/footer-page/footer-page.component.ts
@@ -1,9 +1,10 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer-page.component.html',
   styleUrl: './footer-page.component.css'
 })
