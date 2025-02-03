24102db0ddf95b8f23221e60e8579c11e89448e2
Jean Yves <jeanyvessadio7@gmail.com>
2025-02-03 12:20:08 +0000

    Modification contact et annonce

--- src/app/modules/client/annonces/annonces.component.ts
+++ src/app/modules/client/annonces/annonces.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-annonces',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './annonces.component.html',
   styleUrl: './annonces.component.css'
 })
 export class AnnoncesComponent {
-
+  annonces = [
+    {id: 1, title: 'Annonce 1', content: 'Annonce 1', annee_academique: new Date(), image:"assets/banners/image 9.png"},
+    {id: 2, title: 'Annonce 2', content: 'Annonce 2', annee_academique: new Date(), image:"assets/banners/image 9.png"},
+    {id: 3, title: 'Annonce 3', content: 'Annonce 3', annee_academique: new Date(), image:"assets/banners/image-1.png"},
+    {id: 4, title: 'Annonce 4', content: 'Annonce 4', annee_academique: new Date(), image:"assets/banners/image.png"},
+    {id: 5, title: 'Annonce 5', content: 'Annonce 5', annee_academique: new Date(), image:"assets/banners/image 9.png"},
+    {id: 6, title: 'Annonce 6', content: 'Annonce 6', annee_academique: new Date(), image:"assets/banners/image 9.png"}
+]
 }
