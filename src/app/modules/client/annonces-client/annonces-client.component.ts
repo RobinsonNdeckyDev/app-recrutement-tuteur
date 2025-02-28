579c11d813031d4d9467ed3cdc987f3e8921134d
Jean Yves <jeanyvessadio7@gmail.com>
2025-02-28 22:24:02 +0000

    modification contact

--- src/app/modules/client/annonces-client/annonces-client.component.ts
+++ src/app/modules/client/annonces-client/annonces-client.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { AnnonceService } from '../../../core/services/api/annonce.service';
 import { CommonModule } from '@angular/common';
+import {Annonce} from "../../../core/models/annonce";
 
 @Component({
   selector: 'app-annonces-client',
@@ -9,25 +10,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './annonces-client.component.html',
   styleUrl: './annonces-client.component.css'
 })
-export class AnnoncesClientComponent {
-    tabAnnonces: any = [];
+export class AnnoncesClientComponent implements OnInit {
+    annonces: Annonce[] = [];
 
-    constructor(
-        private annonceService: AnnonceService
-    ){}
+    constructor(private annonceService: AnnonceService) {}
 
-    ngOnInit(){ 
-        this.getAllAnnonces();       
+    ngOnInit(): void {
+        this.getAllAnnonces();
     }
 
-    getAllAnnonces(){
-        this.annonceService.getAnnonces().subscribe(
-            (annonces) => {
-                console.log("Liste des annonces: ", annonces);
+    getAllAnnonces(): void {
+        this.annonceService.getAnnonces().subscribe({
+            next: (data: Annonce[]) => {
+                this.annonces = data;
+                console.log("Annonces reçues :", this.annonces);
             },
-            (error) => {
-                console.log("erreur: ", error);
+            error: (err) => {
+                console.error("Erreur lors de la récupération des annonces :", err);
             }
-        )
+        });
+
     }
 }
